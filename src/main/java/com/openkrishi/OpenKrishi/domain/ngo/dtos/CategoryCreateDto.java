a4e8bb7896a4cf05750b9c725ea21e280e4e3641
mdsaniulbasirsaz <saniulsaj@gmail.com>
2025-08-20 07:54:17 +0600

    feat: Add CategoryCreateDto for category

---
+++ src/main/java/com/openkrishi/OpenKrishi/domain/ngo/dtos/CategoryCreateDto.java
@@ -0,0 +1,12 @@
+package com.openkrishi.OpenKrishi.domain.ngo.dtos;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class CategoryCreateDto {
+    private String categoryName;
+}
